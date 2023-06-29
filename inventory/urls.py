c16e7c17377a392103efcc2d2a1e187ace3471fc
reyalexander <alexandercayromamani@gmail.com>
2023-06-29 12:46:35 -0500

    ADD: New CostumerUser model with new attributes

--- inventory/urls.py
+++ inventory/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("users.urls")),
+    path('user/', include("user.urls")),
     path('clients/', include("clients.urls")),
     path('products/', include("products.urls")),
     path('providers/', include("providers.urls")),
