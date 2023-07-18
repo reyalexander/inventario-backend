499c0cd60a10397c7f8c2b43ac7a65c76a3943dd
reyalexander <alexandercayromamani@gmail.com>
2023-07-18 11:23:36 -0500

    fix: imagen root and filter startwith

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -11,3 +13,4 @@ urlpatterns = [
     path('token/', CustomTokenObtainPairView .as_view(), name='get_token'),
     path('refresh-token/', TokenRefreshView.as_view(), name="refresh_view"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
