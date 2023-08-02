b5a266adc270915bee34f715bc932398cb0d17ed
reyalexander <alexandercayromamani@gmail.com>
2023-08-02 08:42:43 -0500

    fix: Add create list of order_detail object and add a id_client serializer to Order

--- order/views.py
+++ order/views.py
@@ -7,7 +7,14 @@ from django.http import HttpResponse
 from django.views import View
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from io import BytesIO
+from reportlab.platypus import Table, TableStyle, Image
+
 from order_detail.models import OrderDetail
+from user.models import User
 
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -26,6 +33,15 @@ class BoletaPDFView(View):
         response['Content-Disposition'] = f'filename="boleta_{order.order_code}.pdf"'
 
         p = canvas.Canvas(response, pagesize=letter)
+
+        # Obtener la imagen de la empresa y mostrarla en la cabecera de la boleta
+        try:
+            user = User.objects.get(id=2)  # Reemplaza '1' con el ID de la empresa o la forma en que obtienes el usuario/empresa correcto
+            company_image = user.photo
+            if company_image:
+                p.drawImage(company_image.path, 350, 650, width=170, height=90)  # Ajusta las coordenadas según el diseño deseado
+        except User.DoesNotExist:
+            pass
 
         # Configurar los detalles de la boleta
         p.drawString(100, 700, f'Boleta de Orden #{order.order_code}')
@@ -72,4 +88,72 @@ class BoletaPDFView(View):
         p.showPage()
         p.save()
 
+        return response
+    
+
+
+class BoletaCuadradaPDFView(View):
+    def get(self, request, order_id):
+        order = Order.objects.get(pk=order_id)
+        order_details = OrderDetail.objects.filter(id_order=order)
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'filename="boleta_{order.order_code}.pdf"'
+
+        buffer = BytesIO()
+        doc = SimpleDocTemplate(buffer, pagesize=letter)
+
+        # Obtener la imagen de la empresa y mostrarla en la cabecera de la boleta
+        try:
+            user = User.objects.get(id=2)  # Reemplaza '1' con el ID de la empresa o la forma en que obtienes el usuario/empresa correcto
+            company_image = user.photo
+        except User.DoesNotExist:
+            company_image = None
+
+        # Crear la lista de elementos para la cabecera
+        elements = []
+
+        if company_image:
+            image_width, image_height = 170, 100
+            company_image_path = company_image.path
+            image = Image(company_image_path, width=image_width, height=image_height)
+            elements.append(image)
+
+        styles = getSampleStyleSheet()
+        elements.append(Paragraph(f'<b>Cliente:</b> {order.id_client.name}', styles['Normal']))
+        elements.append(Paragraph(f'<b>Número de Boleta:</b> {order.order_code}', styles['Normal']))
+        elements.append(Paragraph(f'<b>Fecha:</b> {order.date.strftime("%d/%m/%Y %H:%M:%S")}', styles['Normal']))
+        elements.append(Spacer(1, 20))  # Espacio en blanco
+
+        # Crear una tabla para mostrar los detalles del pedido
+        table_data = [
+            ['Producto', 'Cantidad', 'Subtotal'],
+        ]
+
+        for detail in order_details:
+            product = detail.id_product.name
+            quantity = str(detail.quantity)
+            subtotal = str(detail.quantity * detail.new_sale_price)
+            table_data.append([product, quantity, subtotal])
+
+        table = Table(table_data, colWidths=[200, 100, 100], hAlign='LEFT', repeatRows=1)
+        table.setStyle(TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ]))
+
+        elements.append(table)
+
+        # Construir el documento y guardar en el buffer
+        doc.build(elements)
+        pdf = buffer.getvalue()
+        buffer.close()
+
+        # Devolver el PDF como respuesta
+        response.write(pdf)
         return response
--- order_detail/views.py
+++ order_detail/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import OrderDetail
 from products.models import Product
 from .serializers import OrderDetailSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -11,6 +12,26 @@ class OrderDetailViewSet(viewsets.ModelViewSet):
     serializer_class = OrderDetailSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        if isinstance(data, list):
+            serializers = [self.get_serializer(data=item) for item in data]
+            valid = all(serializer.is_valid() for serializer in serializers)
+            if valid:
+                order_details = [serializer.save() for serializer in serializers]
+                return Response(OrderDetailSerializer(order_details, many=True).data, status=status.HTTP_201_CREATED)
+            else:
+                errors = [serializer.errors for serializer in serializers]
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        
+        
 
     def perform_create(self, serializer):
         order_detail = serializer.save()
