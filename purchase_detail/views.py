46f48a5679fcfa31847e4ad0323a433b26d292e5
reyalexander <alexandercayromamani@gmail.com>
2023-08-08 11:11:23 -0500

    feat: add purchase detaul create list

--- purchase_detail/views.py
+++ purchase_detail/views.py
@@ -1,6 +1,7 @@
 from .models import PurchaseDetail
 from .serializers import PurchaseDetailSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response 
 from django_filters.rest_framework import DjangoFilterBackend
 
 class PurchaseDetailViewSet(viewsets.ModelViewSet):
@@ -8,3 +9,21 @@ class PurchaseDetailViewSet(viewsets.ModelViewSet):
     serializer_class = PurchaseDetailSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        if isinstance(data, list):
+            serializers = [self.get_serializer(data=item) for item in data]
+            valid = all(serializer.is_valid() for serializer in serializers)
+            if valid:
+                order_details = [serializer.save() for serializer in serializers]
+                return Response(PurchaseDetailSerializer(order_details, many=True).data, status=status.HTTP_201_CREATED)
+            else:
+                errors = [serializer.errors for serializer in serializers]
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
