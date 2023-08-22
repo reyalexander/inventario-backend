5459d69b4a488dce2706c50fac8058ab6ff5beaf
reyalexander <alexandercayromamani@gmail.com>
2023-08-22 14:57:34 -0500

    fix: modified description lenght

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from categories.models import Category
 
 class Product(models.Model):
     code = models.CharField(max_length=150, blank=True)
-    name = models.CharField(max_length=500, null=False)
-    description = models.CharField(max_length=50, default='', blank=True)
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=250, default='', blank=True)
     cost = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     stock = models.IntegerField(default=0)
