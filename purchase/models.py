04bb8dadba4c9ccbd6df52279c0a0f5eb580deb8
reyalexander <alexandercayromamani@gmail.com>
2023-09-12 16:24:49 -0500

    feat: total_price purchase models

--- purchase/models.py
+++ purchase/models.py
@@ -8,6 +8,7 @@ class Purchase(models.Model):
     order_code = models.CharField(max_length=150, blank=True)
     evidence = models.ImageField(upload_to='purchase', blank=True, null=True)
     detail = models.CharField(max_length=250, blank=True)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     date = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False, null=True)
 
